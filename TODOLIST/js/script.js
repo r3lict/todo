d25cfe24806976b34f2cf37152f6215b1d642ea9
Adrian Godlewski <46203433+r3lict@users.noreply.github.com>
2019-06-06 23:57:56 +0200

    Add files via upload

--- TODOLIST/js/script.js
+++ TODOLIST/js/script.js
@@ -5,7 +5,6 @@ let todoSearch = null;
 function addTask(text) {
     console.log('Dodaję zadanie do listy');
 }
-
 document.addEventListener('DOMContentLoaded', function () {
     const todoList = document.querySelector('#todoList');
     const todoForm = document.querySelector('#todoForm');
@@ -22,41 +21,40 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
     function addTask(text) {
-        //element todo
+
         const todo = document.createElement('div');
         todo.classList.add('todo-element');
 
-        //belka górna
+
         const todoBar = document.createElement('div');
         todoBar.classList.add('todo-element-bar');
 
-        //data w belce
+        //DATA
         const todoDate = document.createElement('div');
         todoDate.classList.add('todo-element-bar');
         const date = new Date();
         const dateText = date.getDate() + '-' + (date.getMonth() + 1) + '-' + date.getFullYear() + ' godz.: ' + date.getHours() + ':' + date.getMinutes();
         todoDate.innerText = dateText;
 
-        //przycisk usuwania
+
         const todoDelete = document.createElement('button');
         todoDelete.classList.add('todo-element-delete');
         todoDelete.classList.add('button');
         todoDelete.innerHTML = '<i class="fas fa-times-circle"></i>';
 
-        //wrzucamy elementy do belki
         todoBar.appendChild(todoDate);
         todoBar.appendChild(todoDelete);
 
-        //element z tekstem
+        //tekst
         const todoText = document.createElement('div');
         todoText.classList.add('todo-element-text');
         todoText.innerText = text;
 
-        //łączymy całość
+
         todo.appendChild(todoBar);
         todo.appendChild(todoText);
 
-        //i wrzucamy do listy
+        //LISTA
         todoList.append(todo);
     }
     todoList.addEventListener('click', function (e) {
