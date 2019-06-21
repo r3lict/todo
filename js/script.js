d7b95ee33965a6913cb7993b68a267bf30459413
Adrian Godlewski <46203433+r3lict@users.noreply.github.com>
2019-06-21 20:29:59 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,68 @@
+document.getElementById("portButt").onclick = function () {
+    location.href = "https://github.com/r3lict";
+};
+
+
+let todoList = 0;
+let todoForm = 0;
+
+document.addEventListener('DOMContentLoaded', function () {
+    const todoList = document.querySelector('#todoList');
+    const todoForm = document.querySelector('#todoForm');
+
+
+    todoForm.addEventListener('submit', function (e) {
+        e.preventDefault();
+        const textarea = this.querySelector('textarea');
+        if (textarea.value !== '') {
+            addTask(textarea.value);
+            textarea.value = '';
+        }
+    });
+
+
+    function addTask(text) {
+
+        const todo = document.createElement('div');
+        todo.classList.add('todo-element');
+
+
+        const todoBar = document.createElement('div');
+        todoBar.classList.add('todo-element-bar');
+
+        //DATA
+        const todoDate = document.createElement('div');
+        todoDate.classList.add('todo-element-bar');
+        const date = new Date();
+        const dateText = date.getDate() + '-' + (date.getMonth() + 1) + '-' + date.getFullYear() + ' godz.: ' + date.getHours() + ':' + date.getMinutes();
+        todoDate.innerText = dateText;
+
+
+        const todoDelete = document.createElement('button');
+        todoDelete.classList.add('todo-element-delete');
+        todoDelete.classList.add('button');
+        todoDelete.innerHTML = '<i class="fas fa-times-circle"></i>';
+
+        todoBar.appendChild(todoDate);
+        todoBar.appendChild(todoDelete);
+
+        //tekst
+        const todoText = document.createElement('div');
+        todoText.classList.add('todo-element-text');
+        todoText.innerText = text;
+
+
+        todo.appendChild(todoBar);
+        todo.appendChild(todoText);
+
+        //LISTA
+        todoList.append(todo);
+    }
+    todoList.addEventListener('click', function (e) {
+        if (e.target.closest('.todo-element-delete') !== null) {
+            const todoElem = e.target.closest('.todo-element');
+            todoElem.parentNode.removeChild(todoElem);
+        }
+    });
+
+});
